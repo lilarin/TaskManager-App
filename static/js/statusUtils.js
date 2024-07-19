8a43ea2051ed1ed7152ee647daf937dee41d8350
lilarin <80650911+lilarin@users.noreply.github.com>
2024-07-19 16:26:22 +0300

    Minor style and js improvements

--- static/js/statusUtils.js
+++ static/js/statusUtils.js
@@ -9,12 +9,12 @@ function formatStatus(statusText, deadlineText) {
     const timeDiff = deadlineDate - currentDate;
     const daysLeft = Math.ceil(timeDiff / (1000 * 3600 * 24));
     if (daysLeft > 1) {
-      message = `Pending (${daysLeft} days left)`;
+      message = `Pending: ${daysLeft} days left`;
       if (daysLeft <= 3) {
         color = '#ff6174';
       }
     } else if (daysLeft === 1) {
-      message = 'Pending (1 day left)';
+      message = 'Pending: 1 day left';
       color = '#e83f21';
     } else if (daysLeft === 0) {
       message = 'Pending: Deadline is today';
